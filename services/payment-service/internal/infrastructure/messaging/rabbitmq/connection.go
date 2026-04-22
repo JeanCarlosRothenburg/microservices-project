b3701a647e1f6fe74405e5cbbd3d8aa9e23058a4
jean.rothenburg <jeancarlosrothenburg13@gmail.com>
2026-04-22 13:13:28 -0300

    feat(payment-service): Desenvolvimento da estrutura do broker RabbitMQ

--- services/payment-service/internal/infrastructure/messaging/rabbitmq/connection.go
+++ services/payment-service/internal/infrastructure/messaging/rabbitmq/connection.go
@@ -1,23 +1,75 @@
 package rabbitmq
 
 import (
-	"context"
 	"log"
-	"time"
 	"os"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func failOnError(err error, msg string) -> nil {
+type Connection struct {
+	conn *amqp.Connection
+	channel *amqp.Channel
+	url string
+}
+
+func NewConnection() *Connection {
+	url, exists := os.LookupEnv("RABBITMQ_URL")
+
+	if !exists {
+		log.Fatal("A constante da URL do RabbitMQ URL não foi encontrada!")
+	}
+
+	c := &Connection{url: url}
+	c.connect()
+	return c
+
+}
+
+func (c *Connection) connect() {
+	conn, err := amqp.Dial(c.url)
 	if err != nil {
-		log.Panicf("%v: %v", msg, err)
+		log.Fatal("Não foi possível estabelecer a conexão com o RabbitMQ")
+	}
+
+	channel, err := conn.Channel()
+	if err != nil {
+		conn.Close()
+		log.Fatal("Não foi possível abrir uma canal no RabbitMQ")
+	}
+
+	c.conn = conn
+	c.channel = channel
+
+	err = setup(channel)
+	if err != nil {
+		log.Fatal("Erro ao configurar topologia do RabbitMQ")
 	}
 }
 
-func connect() {
-	rabbitmqURL := os.Getenv("RABBITMQ_URL")
-	conn, err := amqp.Dial(rabbitmqURL)
-	failOnError(err, "Eror ao conectar ao RabbitMQ")
-	defer conn.Close()
+func (c *Connection) Channel() *amqp.Channel {
+	if c.channel == nil || c.channel.IsClosed() {
+		channel, err := c.conn.Channel()
+		if err != nil {
+			log.Println("Erro ao abrir canal no RabbitMQ!")
+			c.connect()
+			return c.channel
+		}
+		log.Println("Canal aberto no RabbitMQ!")
+		c.channel = channel
+
+	}
+
+	log.Println("Já existe um canal aberto no RabbitMQ!")
+	return c.channel
+}
+
+func (c *Connection) Close() {
+	if c.channel != nil && !c.channel.IsClosed() {
+		c.channel.Close()
+	}
+	if c.conn != nil && !c.conn.IsClosed() {
+		c.conn.Close()
+	}
+	log.Println("Conexão com o RabbitMQ encerrada!")
 }
