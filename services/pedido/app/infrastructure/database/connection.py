30b611ab20102f8258433c5c807072fb49cb8704
Guilherme Rode <guilherme.rode77@gmail.com>
2026-04-23 19:23:53 -0300

    Atualização das portas dos microserviços.

--- services/pedido/app/infrastructure/database/connection.py
+++ services/pedido/app/infrastructure/database/connection.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://pedido_user:pedido_pass@localhost:5432/pedido_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://pedido_user:pedido_pass@localhost:5435/pedido_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
