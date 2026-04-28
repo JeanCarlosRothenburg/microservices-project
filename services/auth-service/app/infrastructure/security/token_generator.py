0078e1db4530c5023100b260bf4cb9f9851648e7
jean.rothenburg <jeancarlosrothenburg13@gmail.com>
2026-04-28 14:18:48 -0300

    feat(api-gateway): implementação de autenticação centralizada no API-Gateway

--- services/auth-service/app/infrastructure/security/token_generator.py
+++ services/auth-service/app/infrastructure/security/token_generator.py
@@ -3,17 +3,11 @@ import os
 from datetime import datetime, timezone, timedelta
 import jwt
 
-PRIVATE_KEY_PATH = os.getenv("PRIVATE_KEY_PATH", "/run/secrets/private_key")
-
-
-@lru_cache
-def get_private_key():
-    with open(PRIVATE_KEY_PATH) as file:
-        return file.read()
+JWT_SECRET = os.getenv("JWT_SECRET")
 
 
 def create_token(data: dict):
     payload = data.copy()
     payload["exp"] = datetime.now(timezone.utc) + timedelta(hours=1)
 
-    return jwt.encode(payload, get_private_key(), algorithm="RS256")
+    return jwt.encode(payload, JWT_SECRET, algorithm="HS256")
--- services/estoque/app/infrastructure/security/token_validator.py
+++ services/estoque/app/infrastructure/security/token_validator.py
@@ -3,18 +3,11 @@ import os
 import jwt
 from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
 
-PUBLIC_KEY_PATH = os.getenv("PUBLIC_KEY_PATH", "/run/secrets/public_key")
-
-
-@lru_cache
-def get_public_key() -> str:
-    with open(PUBLIC_KEY_PATH) as file:
-        return file.read()
-
+JWT_SECRET = os.getenv("JWT_SECRET")
 
 def validate_token(token: str) -> dict:
     try:
-        return jwt.decode(token, get_public_key(), algorithms="RS256")
+        return jwt.decode(token, JWT_SECRET, algorithms="HS256")
     except ExpiredSignatureError:
         raise ValueError("Token expirado")
     except InvalidTokenError:
