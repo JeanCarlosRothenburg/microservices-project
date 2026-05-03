40b564b3a1b342761a9b6545d5b781bfc9ed0a22
Guilherme Rode <guilherme.rode77@gmail.com>
2026-05-03 09:37:11 -0300

    Correção da autenticação do estoque

--- services/estoque/app/infrastructure/security/auth_dependency.py
+++ services/estoque/app/infrastructure/security/auth_dependency.py
@@ -6,5 +6,10 @@ from app.infrastructure.security.token_validator import validate_token
 security = HTTPBearer()
 
 
-def get_current_user():
-    return {"email": "dev@local.com"}
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+    try:
+        payload = validate_token(token)
+        return payload
+    except ValueError as e:
+        raise HTTPException(status_code=401, detail=str(e))
