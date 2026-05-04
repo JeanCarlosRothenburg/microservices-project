5a9b928bc0952915eb5568197d48c4432a7f8a6e
Kaique de Oliveira <kaiquedeoliveira0412@gmail.com.br>
2026-05-03 22:43:54 -0300

    feat(pedido): ajutando testes automatizados que haviam quebrados

--- services/pedido/tests/app/stubs/pedido_stub.py
+++ services/pedido/tests/app/stubs/pedido_stub.py
@@ -1,26 +1,13 @@
 import uuid
-
-from app.domain.pedido import ItemPedido, Pedido, StatusPedido
-
-
-def get_itens_stub() -> list[dict]:
-    return [
-        {
-            "produto_sku": "SKU-001",
-            "quantidade": 2,
-            "preco_unitario": 50.0
-        }
-    ]
+from app.domain.pedido import ItemPedido, MetodoPagamento, Pedido, StatusPedido
 
 
 def _criar_itens():
-    return [
-        ItemPedido(
-            produto_sku="SKU-001",
-            quantidade=2,
-            preco_unitario=50.0
-        )
-    ]
+    return [ItemPedido(produto_sku="SKU-001", quantidade=2, preco_unitario=50.0)]
+
+
+def get_itens_stub():
+    return [{"produto_sku": "SKU-001", "quantidade": 2, "preco_unitario": 50.0}]
 
 
 def get_pedido_pendente_stub() -> Pedido:
@@ -28,6 +15,7 @@ def get_pedido_pendente_stub() -> Pedido:
         id=str(uuid.uuid4()),
         usuario_email="user@email.com",
         itens=_criar_itens(),
+        metodo_pagamento=MetodoPagamento.PIX,
         status=StatusPedido.PENDENTE,
     )
 
@@ -37,6 +25,7 @@ def get_pedido_aprovado_stub() -> Pedido:
         id=str(uuid.uuid4()),
         usuario_email="user@email.com",
         itens=_criar_itens(),
+        metodo_pagamento=MetodoPagamento.PIX,
         status=StatusPedido.APROVADO,
     )
 
@@ -46,5 +35,6 @@ def get_pedido_cancelado_stub() -> Pedido:
         id=str(uuid.uuid4()),
         usuario_email="user@email.com",
         itens=_criar_itens(),
+        metodo_pagamento=MetodoPagamento.PIX,
         status=StatusPedido.CANCELADO,
     )
