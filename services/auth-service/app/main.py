5d37b234fd60d147e7fc25d7eb8fddc45ba56827
jean.rothenburg <jeancarlosrothenburg13@gmail.com>
2026-05-27 00:37:16 -0300

    chore(auth-service_: desenvolvimento do pipeline de CI/CD e configuração da disponibilidade do swagger para do serviço

--- services/auth-service/app/main.py
+++ services/auth-service/app/main.py
@@ -1,6 +1,14 @@
+import os
 from fastapi import FastAPI
 from app.controller.auth_controller import router as auth_router
 
-app = FastAPI(root_path="/auth")
+swagger_enabled = os.getenv("SWAGGER_ENABLED", "true").lower() == "true"
+
+app = FastAPI(
+    root_path="/auth",
+    docs_url="/docs" if swagger_enabled else None,
+    redoc_url="/redoc" if swagger_enabled else None,
+    openapi_url="/openapi.json" if swagger_enabled else None,
+)
 
 app.include_router(auth_router)
