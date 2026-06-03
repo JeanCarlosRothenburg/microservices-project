f84dd4f883d750fd1586a452d54ae6d99182d80a
Kaique de Oliveira <kaiquedeoliveira0412@gmail.com>
2026-06-03 19:37:16 -0300

    Update DATABASE_URL for development environment

--- services/pedido/app/infrastructure/database/connection.py
+++ services/pedido/app/infrastructure/database/connection.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://pedido_user:pedido_pass@localhost:5435/pedido_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://pedido_db_dev_z6u8_user:iS24WyCcENsNGlRYCqpPMYVkuZsqtlEM@dpg-d8fo5c58nd3s738r5360-a/pedido_db_dev_z6u8")
 
 if DATABASE_URL:
     engine = create_engine(DATABASE_URL)
