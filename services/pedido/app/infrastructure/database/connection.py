cf1e92ff466f6ba16299786537c65d99c250b32d
Kaique de Oliveira <kaiquedeoliveira0412@gmail.com>
2026-06-03 19:22:36 -0300

    feat(pedido): ajustando conexão do bd

--- services/pedido/app/infrastructure/database/connection.py
+++ services/pedido/app/infrastructure/database/connection.py
@@ -4,8 +4,9 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://pedido_user:pedido_pass@localhost:5435/pedido_db")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-
-class Base(DeclarativeBase):
-    pass
+if DATABASE_URL:
+    engine = create_engine(DATABASE_URL)
+    SessionLocal = sessionmaker(bind=engine)
+    
+    class Base(DeclarativeBase):
+        pass
